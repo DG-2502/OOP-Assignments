66065494307fcd81f538b33b66932d760033d06c
DG-2502 <dgul7769@gmail.com>
2026-01-23 19:12:33 +0500

    3

---
+++ Assignment_3/Task_2/src/PizzaDecorator.java
@@ -0,0 +1,16 @@
+public abstract class PizzaDecorator implements Pizza{
+    private final Pizza decoratedPizza;
+    public PizzaDecorator(Pizza pizza){
+        this.decoratedPizza = pizza;
+    }
+
+    @Override
+    public double getCost() {
+        return decoratedPizza.getCost();
+    }
+
+    @Override
+    public String getIngredients() {
+        return decoratedPizza.getIngredients();
+    }
+}
